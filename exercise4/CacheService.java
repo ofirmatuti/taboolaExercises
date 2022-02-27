9721f3173ece6ff27dcfca187979d0299bac0c8b
ofir matuti <ofirmatuti@gmail.com>
2022-02-27 18:26:55 +0200

    design decision

--- exercise4/CacheService.java
+++ exercise4/CacheService.java
@@ -2,14 +2,18 @@ package exercise4;
 
 import java.util.Random;
 
-public class CacheService extends LRUCache<Integer,Integer> {
+public class CacheService {
+
+    ICache<Integer,Integer> cache;
+    int capacity;
 
     public CacheService(int capacity) {
-        super(capacity);
+        this.capacity = capacity;
+        cache = new LRUCache<Integer,Integer>(capacity);
     }
 
     public Integer getData(Integer element) throws InterruptedException {
-        Integer res = cacheGet(element);
+        Integer res = cache.cacheGet(element);
         if(res == null){
             System.out.println("executing a network request to the database");
             Thread.sleep(200);
@@ -20,6 +24,6 @@ public class CacheService extends LRUCache<Integer,Integer> {
     }
 
     private void setData(Integer key, Integer val){
-        cacheSet(key, val);
+        cache.cacheSet(key, val);
     }
 }
